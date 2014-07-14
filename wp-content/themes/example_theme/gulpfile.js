b91185035fdff06511528f2c0fc271ef0865c045
jolangreen <jolangreen@hotmail.com>
2014-07-13 23:38:00 -0400

    gulp

---
+++ wp-content/themes/example_theme/gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    compass = require('gulp-compass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload');
+
+
+// Compile Sass using compass
+gulp.task('styles', function() {
+  return gulp.src('sass/main.sass')
+    .pipe(compass({ config_file: './config.rb', css: 'css', sass: 'sass' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+
+//Scripts
+gulp.task('scripts', function() {
+  return gulp.src('js/*.js')
+    //.pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+
+//Images
+gulp.task('images', function() {
+  return gulp.src('images/**/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+
+// Clean. Delete and replace all files in the destination folder.
+gulp.task('clean', function() {
+  return gulp.src(['dist/css', 'dist/js', 'dist/images'], {read: false})
+    .pipe(clean());
+});
+
+
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('sass/*.sass', ['styles']);
+
+  // Watch .js files
+  gulp.watch('js/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('images/**/*', ['images']);
+
+  // Create LiveReload server
+  var server = livereload();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', function(file) {
+    server.changed(file.path);
+  });
+
+});
