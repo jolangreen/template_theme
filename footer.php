34980a524a68d6b7df936cf69194199aebda7971
jolangreen <jolangreen@hotmail.com>
2014-10-20 09:48:48 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -12,10 +12,6 @@
     </div>
 </footer>
 
-<!-- bower:js -->
-<script src="bower_components/jquery/dist/jquery.js"></script>
-<script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
-<!-- endbower -->
 
 <script src="<?php bloginfo('template_directory'); ?>/assets/js/scripts.js"></script>
 <script type="text/javascript">
