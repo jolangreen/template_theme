34b4ee06f4556ee7a1aa9911a2d2cc10ce12ab59
jolangreen <jolangreen@hotmail.com>
2014-10-21 16:28:09 -0400

    Update page.php

--- page.php
+++ page.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
 
+    <section class="page-intro">
+        <div class="tint">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1>Page Title</h1>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="breadcrumb"><a href="<?php echo home_url( '/' ); ?>">Home</a> / <span class="active">About</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
         <section>
             <div class="row">
                 <div class="primary col-md-8">
