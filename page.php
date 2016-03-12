523892ecef287663eca8e37fff488eb49e0e5f10
Jolan Green <jolangreen7@gmail.com>
2016-03-12 01:09:42 -0500

    Update page.php

--- page.php
+++ page.php
@@ -16,20 +16,22 @@
     </section>
     
     <section>
-        <div class="row">
-            <div class="primary col-md-8">
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <article>
-    	                <h2><?php the_title(); ?></h2>
-    	                <?php the_content(); ?>
-    	                <?php //comments_template(); ?>
-                	</article>
-                <?php endwhile; ?>
-            </div><!-- .primary -->
-            <div class="secondary col-md-4">
-                <?php get_sidebar(); ?>
-            </div><!-- .secondary -->
-        </div><!-- /.row -->
+        <div class="container">
+            <div class="row">
+                <div class="primary col-md-8">
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <article>
+        	                <h2><?php the_title(); ?></h2>
+        	                <?php the_content(); ?>
+        	                <?php //comments_template(); ?>
+                    	</article>
+                    <?php endwhile; ?>
+                </div><!-- .primary -->
+                <div class="secondary col-md-4">
+                    <?php get_sidebar(); ?>
+                </div><!-- .secondary -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
     </section><!-- /.container -->
 
 <?php get_footer(); ?>
