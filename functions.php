d87a6d6ee9461e2e5238f91e1f5fdcb3080e0fb5
Jolan Green <jolangreen7@gmail.com>
2020-01-01 10:01:08 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -317,8 +317,8 @@ function twentyseventeen_widgets_init() {
 		'description'   => __( 'Add widgets here to appear in your sidebar.', 'twentyseventeen' ),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h4 class="widget-title text-medium-large">',
+		'after_title'   => '</h4>',
 	) );
 
 	register_sidebar( array(
@@ -327,8 +327,8 @@ function twentyseventeen_widgets_init() {
 		'description'   => __( 'Add widgets here to appear in your footer.', 'twentyseventeen' ),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h4 class="widget-title text-medium-large">',
+		'after_title'   => '</h4>',
 	) );
 
 	register_sidebar( array(
@@ -337,8 +337,8 @@ function twentyseventeen_widgets_init() {
 		'description'   => __( 'Add widgets here to appear in your footer.', 'twentyseventeen' ),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h4 class="widget-title text-medium-large">',
+		'after_title'   => '</h4>',
 	) );
 }
 add_action( 'widgets_init', 'twentyseventeen_widgets_init' );
