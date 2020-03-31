9897fd4ad024a26679b562cedb6af4a857756a1f
jolang-github <jolan.green@formstack.com>
2020-03-31 08:18:51 -0400

    updating WP plugin zip

--- 404.php
+++ 404.php
@@ -12,29 +12,22 @@
 
 get_header(); ?>
 
-	<section>
-		<div class="container">
-          	<div class="row">
-                <div class="primary col-md-8">
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="primary col-md-12">
+                <div class="page-wrapper">
+                    <h2><?php _e( '404 ERROR: PAGE NOT FOUND', 'twentyseventeen' ); ?></h2>
+                    <p><?php _e( 'We&rsquo;re sorry, but the page you&rsquo;re looking for can&rsquo;t be found.', 'twentyseventeen' ); ?></p>
+                    <p>You might want to head to our <a href="<?php echo home_url( '/' ); ?>">Home Page</a> to see if you can find what you are looking for from there.</p>
+                    
+                    <p><small>Please report any broken links to the <a href="<?php echo home_url( '/' ); ?>">Team</a>.</p>
 
-        			<div class="page-wrapper">
-        				<div class="page-content">
-        					<h2><?php _e( '404 ERROR: PAGE NOT FOUND', 'twentyseventeen' ); ?></h2>
-        					<p><?php _e( 'We&rsquo;re sorry, but the page you&rsquo;re looking for can&rsquo;t be found.', 'twentyseventeen' ); ?></p>
-        					<p>You might want to head to our <a href="<?php echo home_url( '/' ); ?>">Home Page</a> to see if you can find what you are looking for from there.</p>
-        					
-        					<p><small>Please report any broken links to the <a href="<?php echo home_url( '/' ); ?>">Team</a>.</p>
-
-        					<?php get_search_form(); ?>
-        				</div><!-- .page-content -->
-        			</div><!-- .page-wrapper -->
-
-        		</div><!-- .primary -->
-                <div class="secondary col-md-4">
-                    <?php get_sidebar(); ?>
-                </div><!-- .secondary -->
-            </div><!-- /.row -->
-        </div><!-- /.container -->
-    </section>
+                    <?php //get_search_form(); ?>
+                </div><!-- .page-wrapper -->
+            </div><!-- .primary -->
+        </div><!-- /.row -->
+    </div><!-- /.container -->
+</section>
 
 <?php get_footer();
